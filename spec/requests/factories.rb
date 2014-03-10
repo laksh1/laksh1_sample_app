b1d3f7b3ba98fe5565fd77f2b8ec72b4dcd6ded7
laksh1 <laksh1@umbc.edu>
2014-03-10 01:38:04 -0400

    Add user microposts

---
+++ spec/requests/factories.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com" }
+    password  "secret"
+    password_confirmation "secret"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+end
